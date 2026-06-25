e84a953a1a9f4c243172c269af5296b8897134e4
Aman Kumar <amankrgaya7@gmail.com>
2026-06-25 14:06:43 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,3 +1,4 @@
+import os
 from flask import Flask, render_template, request, jsonify
 from chatbot import respond
 
@@ -19,4 +20,8 @@ def chat():
     })
 
 if __name__ == "__main__":
-    app.run(debug=True)
+      app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT", 5000)),
+        debug=False
+    )
